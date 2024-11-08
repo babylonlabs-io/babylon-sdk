2e88e7bda44eea0e29ecd0b01cc468f4d73a281c
Mauro Lacy <mauro@lacy.com.es>
2024-11-08 10:47:18 +0700

    Add grpc queries support / whitelist

---
+++ demo/app/wasm/grpc_whitelist.go
@@ -0,0 +1,29 @@
+package wasm
+
+import (
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/codec"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+)
+
+func RegisterGrpcQueries(bApp *baseapp.BaseApp, appCodec codec.Codec) []wasmkeeper.Option {
+	queryRouter := bApp.GRPCQueryRouter()
+	queryPluginOpt := wasmkeeper.WithQueryPlugins(
+		&wasmkeeper.QueryPlugins{
+			Stargate: wasmkeeper.AcceptListStargateQuerier(WhitelistedGrpcQuery(), queryRouter, appCodec),
+			Grpc:     wasmkeeper.AcceptListGrpcQuerier(WhitelistedGrpcQuery(), queryRouter, appCodec),
+		})
+
+	return []wasmkeeper.Option{
+		queryPluginOpt,
+	}
+}
+
+// WhitelistedGrpcQuery returns the whitelisted Grpc queries
+func WhitelistedGrpcQuery() wasmkeeper.AcceptedQueries {
+	return wasmkeeper.AcceptedQueries{
+		// mint
+		"/cosmos.mint.v1beta1.Query/Params": &minttypes.QueryParamsResponse{},
+	}
+}
