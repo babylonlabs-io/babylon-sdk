5d6d18b818104e5fe51765f635733522280ad180
Gurjot <gurjotsyd@gmail.com>
2025-05-08 16:06:35 -0400

    fix params

--- x/babylon/types/params.go
+++ x/babylon/types/params.go
@@ -11,54 +11,6 @@ import (
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 )
 
-// ValidateBasic performs basic validation on babylon parameters.
-func (p Params) ValidateBasic() error {
-	if p.MaxGasBeginBlocker == 0 {
-		return ErrInvalid.Wrap("empty max gas end-blocker setting")
-	}
-	return nil
-}
-
-func (p Params) GetContractAddresses() (sdk.AccAddress, sdk.AccAddress, sdk.AccAddress, sdk.AccAddress, error) {
-	if !p.IsCodeStored() {
-		return nil, nil, nil, nil, errors.New("contracts are not instantiated")
-	}
-
-	babylonAddr, err := sdk.AccAddressFromBech32(p.BabylonContractAddress)
-	if err != nil {
-		return nil, nil, nil, nil, err
-	}
-	btcLightClientAddr, err := sdk.AccAddressFromBech32(p.BtcLightClientContractAddress)
-	if err != nil {
-		return nil, nil, nil, nil, err
-	}
-	btcStakingAddr, err := sdk.AccAddressFromBech32(p.BtcStakingContractAddress)
-	if err != nil {
-		return nil, nil, nil, nil, err
-	}
-	btcFinalityAddr, err := sdk.AccAddressFromBech32(p.BtcFinalityContractAddress)
-	if err != nil {
-		return nil, nil, nil, nil, err
-	}
-
-	return babylonAddr, btcLightClientAddr, btcStakingAddr, btcFinalityAddr, nil
-}
-
-func (p Params) IsCodeStored() bool {
-	return p.BabylonContractCodeId != 0 &&
-		p.BtcLightClientContractCodeId != 0 &&
-		p.BtcStakingContractCodeId != 0 &&
-		p.BtcFinalityContractCodeId != 0
-}
-
-func (p Params) IsContractInstantiated() bool {
-	return p.IsCodeStored() &&
-		len(p.BabylonContractAddress) > 0 &&
-		len(p.BtcLightClientContractAddress) > 0 &&
-		len(p.BtcStakingContractAddress) > 0 &&
-		len(p.BtcFinalityContractAddress) > 0
-}
-
 var _ paramtypes.ParamSet = (*Params)(nil)
 
 // ParamKeyTable the param key table for launch module
@@ -101,5 +53,49 @@ func (p Params) Validate() error {
 		return fmt.Errorf("sum of all portions should be less than 1")
 	}
 
+	if p.MaxGasBeginBlocker == 0 {
+		return fmt.Errorf("empty max gas end-blocker setting")
+	}
+
 	return nil
+}
+
+func (p Params) GetContractAddresses() (sdk.AccAddress, sdk.AccAddress, sdk.AccAddress, sdk.AccAddress, error) {
+	if !p.IsCodeStored() {
+		return nil, nil, nil, nil, errors.New("contracts are not instantiated")
+	}
+
+	babylonAddr, err := sdk.AccAddressFromBech32(p.BabylonContractAddress)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	btcLightClientAddr, err := sdk.AccAddressFromBech32(p.BtcLightClientContractAddress)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	btcStakingAddr, err := sdk.AccAddressFromBech32(p.BtcStakingContractAddress)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	btcFinalityAddr, err := sdk.AccAddressFromBech32(p.BtcFinalityContractAddress)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	return babylonAddr, btcLightClientAddr, btcStakingAddr, btcFinalityAddr, nil
+}
+
+func (p Params) IsCodeStored() bool {
+	return p.BabylonContractCodeId != 0 &&
+		p.BtcLightClientContractCodeId != 0 &&
+		p.BtcStakingContractCodeId != 0 &&
+		p.BtcFinalityContractCodeId != 0
+}
+
+func (p Params) IsContractInstantiated() bool {
+	return p.IsCodeStored() &&
+		len(p.BabylonContractAddress) > 0 &&
+		len(p.BtcLightClientContractAddress) > 0 &&
+		len(p.BtcStakingContractAddress) > 0 &&
+		len(p.BtcFinalityContractAddress) > 0
 }
