2d9d9aa4fad38b59dbf0a9de22a98be79ab2ade6
Mauro Lacy <mauro@lacy.com.es>
2025-07-18 11:46:22 +0200

    Update local contracts path

--- tests/scripts/copy_local_wasm.sh
+++ tests/scripts/copy_local_wasm.sh
@@ -2,16 +2,17 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
+CONTRACTS_FOLDER="cosmos-bsn-contracts"
 CONTRACTS="babylon_contract btc_light_client btc_staking btc_finality"
 OUTPUT_FOLDER="$(dirname "$0")/../testdata"
 
 echo "DEV-only: copy from local built instead of downloading"
 
 for CONTRACT in $CONTRACTS; do
-  cp -f ../../babylon-contract/artifacts/"${CONTRACT}".wasm "$OUTPUT_FOLDER/"
+  cp -f "../../${CONTRACTS_FOLDER}/artifacts/${CONTRACT}".wasm "$OUTPUT_FOLDER/"
 done
 
-cd ../../babylon-contract
+cd "../../${CONTRACTS_FOLDER}"
 TAG=$(git rev-parse HEAD)
 cd - 2>/dev/null
 echo "$TAG" >"$OUTPUT_FOLDER/version.txt"
